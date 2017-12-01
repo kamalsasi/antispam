3d8e5de26d780f512e43f35215358d623a610e71
kamalsasi <sasi298@yahoo.com>
2017-12-01 07:49:50 +0330

    Update time1.lua

--- plugins/time1.lua
+++ plugins/time1.lua
@@ -1,6 +1,6 @@
 --Start By edit @mohammadrezajijji
 function run(msg, matches)
-local url , res = http.request('http://api.gpmod.ir/time/')
+local url , res = http.request('http://irapi.ir/time')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
 local text = '*⏰Ir Time:* _'..jdat.FAtime..'_\n*📆Ir Data:* _'..jdat.FAdate..'_\n------------\n*⏰En Time:* _'..jdat.ENtime..'_\n *📆En Data:* _'..jdat.ENdate.. '_\n'
